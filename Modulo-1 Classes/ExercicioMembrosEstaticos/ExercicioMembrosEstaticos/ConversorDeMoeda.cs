769dc13b46b0392a4d85ce597331c3b62d703594
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-31 20:14:15 -0300

    Add: Exercicio sobre metodos estaticos

---
+++ Modulo-1 Classes/ExercicioMembrosEstaticos/ExercicioMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ExercicioMembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        public static double iof = 6.0;
+
+        public static double ConvertDolarParaReal(double quantidade, double cotacao)
+        {
+            double total = quantidade * cotacao;
+            return total + total * iof / 100.00;
+        }
+    }
+}
---
+++ Modulo-1 Classes/ExercicioMembrosEstaticos/ExercicioMembrosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioMembrosEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double quantidadeDeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.ConvertDolarParaReal(quantidadeDeDolar, cotacaoDolar);
+
+            Console.WriteLine($"Valor em R$ {resultado.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
