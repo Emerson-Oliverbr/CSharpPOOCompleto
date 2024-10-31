6312b9e1639f711549df9d86d65930342b4aa73d
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-10-31 15:24:10 -0300

    Aprendendo metodos estaticos parte-3

---
+++ Modulo-1 Classes/MembrosEstaticosParte3/MembrosEstaticosParte3/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace MembrosEstaticosParte3
+{
+    internal class Calculadora
+    {
+        public static double pi = 3.14;
+        public static double Circuferencia(double raio)
+        {
+            return 2.0 * pi * raio;
+        }
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * pi * Math.Pow(raio, 3);
+        }
+    }
+}
