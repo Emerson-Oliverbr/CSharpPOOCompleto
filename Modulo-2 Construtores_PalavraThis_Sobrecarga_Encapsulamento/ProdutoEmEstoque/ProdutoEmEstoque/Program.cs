eb71dfb2dedf264cca9288f345c6d290b4ea2675
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-01 20:20:39 -0300

    Aprendendo Construtores

---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/ProdutoEmEstoque/ProdutoEmEstoque/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosEmEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: em Estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine($"Dados do Produto: {produto}");
+            Console.Write("Informe a quantidade de Produtos a serem adicionadas ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qtde);
+            Console.WriteLine($"Dados Atualizados: {produto}");
+            Console.Write("Informe a quantidade de Produtos a serem removidas ao estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qtde);
+            Console.WriteLine($"Dados Atualizados: {produto}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/ProdutoEmEstoque/README.md
@@ -0,0 +1,15 @@
+# Exercício: Estoque
+
+## Descrição do Projeto
+Este é um exercício para praticar conceitos de Orientação a Objetos em C#, envolvendo a criação de uma classe que representa um produto no estoque de uma empresa. O programa desenvolvido permite realizar operações de entrada e saída no estoque, além de exibir informações atualizadas do produto.
+
+## Funcionalidades
+O programa possui as seguintes funcionalidades:
+- Ler os dados de um produto em estoque, incluindo:
+  - Nome do produto
+  - Preço unitário
+  - Quantidade em estoque
+- Exibir as informações do produto (nome, preço, quantidade no estoque e valor total no estoque)
+- Realizar uma entrada no estoque, atualizando a quantidade de produtos
+- Realizar uma saída no estoque, atualizando a quantidade de produtos
+
