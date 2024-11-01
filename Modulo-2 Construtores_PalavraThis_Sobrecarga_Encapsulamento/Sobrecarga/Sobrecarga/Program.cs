65aa8226a4eb34f03dac45caa9496e95a757aff2
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-01 20:35:58 -0300

    Aprendendo Sobrecarga

---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/Sobrecarga/Sobrecarga/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosEmEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+
+            Produto produto = new Produto(nome, preco);
+            Produto produto2 = new Produto();
+
+            Console.WriteLine($"Dados do Produto: {produto}");
+            Console.Write("Informe a quantidade de Produtos a serem adicionadas ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qtde);
+            Console.WriteLine($"Dados Atualizados: {produto}");
+            Console.Write("Informe a quantidade de Produtos a serem removidas ao estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qtde);
+            Console.WriteLine($"Dados Atualizados: {produto}");
+
+            Console.ReadKey();
+        }
+    }
+}
