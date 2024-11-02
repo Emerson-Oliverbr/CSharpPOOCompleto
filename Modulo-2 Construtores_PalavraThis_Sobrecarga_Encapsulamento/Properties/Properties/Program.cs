4ededb46827ed701697fb7d99dafdc1d87ac6b7c
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-02 16:27:32 -0300

    Aprendendo Properties

---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/Properties/Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            produto.Nome = "TV-4K";
+
+            Console.WriteLine($"Nome: {produto.Nome}");
+            Console.WriteLine($"Preço: {produto.Preco}");
+
+            Console.ReadKey();
+        }
+    }
+}
