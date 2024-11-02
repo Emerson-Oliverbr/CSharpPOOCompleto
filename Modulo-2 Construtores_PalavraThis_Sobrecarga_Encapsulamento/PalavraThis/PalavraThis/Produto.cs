e9a5eba6be03478374d3fe0794db0245883563bf
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-02 11:45:05 -0300

    Add: Palavra this

---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/PalavraThis/PalavraThis/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosEmEstoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return $"{Nome}, R$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, "
+            + $"{Quantidade} unidades, Total R$ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/PalavraThis/PalavraThis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosEmEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Produto produto = new Produto(nome, preco);
+            //Produto produto2 = new Produto();
+
+            Produto produto3 = new Produto() { Nome = "TV", Preco = 500.00, Quantidade = 5 };
+
+            Console.WriteLine($"Dados do Produto: {produto3}");
+            Console.Write("Informe a quantidade de Produtos a serem adicionadas ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            produto3.AdicionarProdutos(qtde);
+            Console.WriteLine($"Dados Atualizados: {produto3}");
+            Console.Write("Informe a quantidade de Produtos a serem removidas ao estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            produto3.RemoverProdutos(qtde);
+            Console.WriteLine($"Dados Atualizados: {produto3}");
+
+            Console.ReadKey();
+        }
+    }
+}
