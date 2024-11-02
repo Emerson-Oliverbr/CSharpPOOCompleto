aedb07819c64c205a9f2f0649bacea330a150099
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-02 12:53:07 -0300

    Aprendendo encapsulamento

---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine($"Nome = {produto.GetNome()}");
+            produto.SetNome("Notebook");
+            Console.WriteLine($"Nome = {produto.GetNome()}");
+            Console.WriteLine($"Preco = {produto.GetPreco()}");
+            Console.WriteLine($"Quantidade = {produto.GetQuantidade()}");
+
+            Console.ReadKey();
+        }
+    }
+}
