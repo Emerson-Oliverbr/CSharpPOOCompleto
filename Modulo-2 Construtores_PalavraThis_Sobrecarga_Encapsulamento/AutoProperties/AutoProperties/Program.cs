e0ba4beaba80e6d3c715f8802d27b8d20f2ecbb7
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-16 20:36:19 -0300

    Apredendo as auto properties

---
+++ Modulo-2 Construtores_PalavraThis_Sobrecarga_Encapsulamento/AutoProperties/AutoProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AutoProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            produto.Nome = "TV-4K";
+
+            Console.WriteLine($"Nome: {produto.Nome}");
+            Console.WriteLine($"Preço: {produto.Preco}");
+
+            Console.ReadKey();
+        }
+    }
+}
