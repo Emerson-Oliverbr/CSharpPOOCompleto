d404cd60eb386618a96281449b7be1faedad0eb5
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-09 17:27:41 -0300

    Revisando os conceitos de vetores parte 1

---
+++ Modulo-3 Comportamento de memôria, arrays e listas/VetoresParte1/VetoresParte1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetoresParte1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] height = new double[n];
+           
+            for(int i=0; i<n; i++)
+            {
+                height[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                
+            }
+
+            double sum = 0.0;
+            for(int i=0; i<n; i++)
+            {
+                sum += height[i];
+            }
+
+            double averageHeight = sum / n;
+
+            Console.WriteLine($"Altura media = {averageHeight.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
