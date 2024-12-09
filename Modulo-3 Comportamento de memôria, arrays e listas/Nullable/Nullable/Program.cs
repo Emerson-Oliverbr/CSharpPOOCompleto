d8ce4763d2206e8a6b57e2f920b8ab530f50af3a
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-09 16:48:14 -0300

    Aprendendo Tipo Nullable

---
+++ Modulo-3 Comportamento de memôria, arrays e listas/Nullable/Nullable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null;
+            double? y = null;
+            double? z = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            /*Retorna valor de x caso não exista nesse caso "null"
+              retorna o valor padrão so tipo da variavel no caso de double 0 
+             */
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            Console.WriteLine(z.GetValueOrDefault());
+            Console.WriteLine();
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+            Console.WriteLine(z.HasValue);
+            Console.WriteLine();
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);                
+            }
+            else
+            {
+                Console.WriteLine($"X é nulo");
+            }
+        }
+    }
+}
