a602ad7bb136aac8422770de855cfeb1bc4a88b2
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-09 22:54:02 -0300

    Revisando os conceitos de vetores parte 2

---
+++ Modulo-3 Comportamento de memôria, arrays e listas/VetoresParte2/VetoresParte2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using VetoresParte2;
+
+namespace VetoresParte1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] products = new Product[n];
+
+            for(int index=0; index<n; index+=1)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                products[index] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+
+            for (int index = 0; index < n; index += 1)
+            {
+                sum += products[index].Price; 
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"Preço medio = {avg.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
